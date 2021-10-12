c3cd2fc106f4fc03a8970572e6d3eee01ad2413c
Matheus Santos <matheus.santos@telergystudio.com>
2021-10-12 15:53:57 -0300

    [CODE] - Agora analisa a quantidade de elementos em 'specification' também.

--- json-converter.py
+++ json-converter.py
@@ -2,23 +2,25 @@ import json
 
 
 def main():
-    with open('sources/prod_parsed.json') as file:
+    with open('prod_parsed-original_para_trabalhar.json', encoding='utf-8') as file:
         # Carrega o arquivo json em um buffer.
         buffer = json.load(file)
-    with open('sources/output.json', 'w') as output:
+    with open('output.json', 'w', encoding='utf-8') as output:
         output_list = []  # Lista vazia para população do arquivo de saída.
         for element in buffer:
             data = {}  # Dicionário vazio para população de cada objeto do json.
             for key in element:
                 # Iterando sobre as chaves do json carregado.
-                if key not in ["auditors", "url", "contract_signatures", "test_signatures", "tools"]:
+                if key not in ["url", "contract_signatures", "test_signatures", "tools"]:
                     # Lista acima representa os dados que não são necessários.
                     if key == 'repos':
                         data[key] = len(element['repos'])
+                    elif key == 'specification':
+                        data[key] = len(element['specification'])
                     else:
                         data[key] = element[key]
             output_list.append(data)
-        json.dump(output_list, output)
+        json.dump(output_list, output, indent=4)
 
 
 if __name__ == '__main__':
