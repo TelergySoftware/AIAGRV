c3c92e3279d1f040096712bc63a00cbf0ea33502
Matheus Santos <matheus.santos@telergystudio.com>
2021-11-09 19:10:26 -0300

    [DB] — Atualização no script de criação do DB.
    Esse novo script adiciona a nova tabela issues e
     a tabela multi relacional auditors_issues para
      fazer a conexão many to many entre issues e
       auditors.

--- sources/utils/resources/DB/CREATE_DB.sql
+++ sources/utils/resources/DB/CREATE_DB.sql
@@ -1,13 +1,37 @@
+CREATE TABLE issues (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        title TEXT NOT NULL,
+                        repos INTEGER NOT NULL,
+                        type TEXT NOT NULL,
+                        start_date DATE NOT NULL,
+                        end_date DATE NOT NULL,
+                        auditors_count INTEGER NOT NULL,
+                        specifications INTEGER NOT NULL,
+                        published INTEGER NOT NULL,
+                        findings_count INTEGER NOT NULL
+);
+
 CREATE TABLE auditors (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            name TEXT NOT NULL
-                        );
+                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                          name TEXT NOT NULL
+);
 
 CREATE TABLE findings (
-                            id INTEGER PRIMARY KEY AUTOINCREMENT,
-                            title TEXT,
-                            severity TEXT NOT NULL,
-                            auditor_id INTEGER NOT NULL,
+                          id INTEGER PRIMARY KEY AUTOINCREMENT,
+                          title TEXT,
+                          severity TEXT NOT NULL,
+                          auditor_id INTEGER NOT NULL,
+                          issue_id INTEGER NOT NULL,
 
-                            FOREIGN KEY (auditor_id) REFERENCES id
-                        );
+                          FOREIGN KEY (auditor_id) REFERENCES id,
+                          FOREIGN KEY (issue_id) REFERENCES id
+);
+
+CREATE TABLE auditors_issues (
+                                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                   auditor_id INTEGER NOT NULL,
+                                   issue_id INTEGER NOT NULL,
+
+                                   FOREIGN KEY (auditor_id) REFERENCES id,
+                                   FOREIGN KEY (issue_id) REFERENCES id
+);
