c74b71b4ed9118b9b9e5818b07320e92d99ed566
Matheus Santos <matheus.santos@telergystudio.com>
2021-11-09 19:07:48 -0300

    [CODE] — Adição do método get_issues na API.
    Foi adicionado o novo método get_issues na API para realizar as procuras de forma mais fácil.
    Ainda existem alguns pontos para serem implementados neste método.

--- sources/utils/src/data_manager.py
+++ sources/utils/src/data_manager.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 from json_converter import convert
+import numpy as np
+import pandas as pd
 from os.path import exists
 import os
 
 import sqlite3
+
+sqlite3.register_adapter(np.int64, lambda val: int(val))
+sqlite3.register_adapter(np.int32, lambda val: int(val))
 
 
 def json_to_sql(json_data: str | list, db_path: str, is_new: bool) -> bool:
@@ -46,63 +51,85 @@ def json_to_sql(json_data: str | list, db_path: str, is_new: bool) -> bool:
     with sqlite3.connect(db_path) as db:
         # Conectamos ao banco já criado.
         cursor = db.cursor()
-        auditors = set()  # Set vazio com os nomes dos auditores.
-        findings = {'title': [], 'severity': [], 'auditor': []}  # Dicionário para estruturado para a tabela findings.
-        for data in json_data:
-            if len(data['auditors']) == 0:
+
+        # Dicionário estruturado para a tabela issues.
+        issues = {'id': [], 'title': [], 'repos': [], 'type': [], 'start_date': [], 'end_date': [],
+                  'auditors_count': [], 'specifications': [], 'published': [], 'findings_count': []}
+        auditors = {'name': [], 'issue_id': []}  # Dicionário estruturado para a tabela auditors.
+        findings = {'title': [], 'severity': [],
+                    'auditor': [], 'issue_id': []}  # Dicionário estruturado para a tabela findings.
+
+        for issue_id, data in enumerate(json_data):
+            issues['id'].append(issue_id + 1)
+            issues['title'].append(data['title'])
+            issues['repos'].append(data['repos'])
+            issues['type'].append(data['type'])
+            issues['start_date'].append(data['start_date'])
+            issues['end_date'].append(data['end_date'])
+            issues['auditors_count'].append(int(len(data['auditors'])))
+            issues['specifications'].append(data['specification'])
+            issues['published'].append(1 if data['published'] == 'true' else 0)
+            issues['findings_count'].append(int(len(data['findings'])))
+
+            if int(len(data['auditors'])) == 0:
                 # Atribui o nome None aos elementos sem auditores.
-                auditors.add('None')
+                auditors['name'].append('None')
+                auditors['issue_id'].append(issue_id + 1)
                 for report in data['findings']:
                     # Popula o dicionário findings.
                     findings['title'].append(report['title'])
                     findings['severity'].append(report['severity'])
                     findings['auditor'].append('None')
+                    findings['issue_id'].append(issue_id + 1)
             else:
                 for auditor in data['auditors']:
-                    auditors.add(auditor) if not auditor.endswith(' ') else auditors.add(auditor[:-1])
+                    auditors['name'].append(auditor) if not auditor.endswith(' ') else auditors['name'].append(auditor[:-1])
+                    auditors['issue_id'].append(issue_id + 1)
+
                     for report in data['findings']:
                         # Popula o dicionário findings.
                         findings['title'].append(report['title'])
                         findings['severity'].append(report['severity'])
                         findings['auditor'].append(auditor) if not auditor.endswith(' ') else \
                             findings['auditor'].append(auditor[:-1])
+                        findings['issue_id'].append(issue_id + 1)
 
-        for auditor in auditors:
-            # Adiciona os auditores na tabela auditors do DB.
+        issues_df = pd.DataFrame(issues)
+        auditors_df = pd.DataFrame(auditors)
+        findings_df = pd.DataFrame(findings)
+
+        for issue in issues_df.iloc():
             sql_script = """
-            INSERT INTO auditors(name) VALUES (?);
+            INSERT INTO issues(title, repos, type, start_date, end_date, auditors_count,
+            specifications, published, findings_count) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
             """
-            cursor.execute(sql_script, [auditor])
+            cursor.execute(sql_script, issue.values[1:])
             db.commit()
 
-        for title, severity, auditor in zip(findings['title'], findings['severity'], findings['auditor']):
+        auditors_set = set()
+        for auditor in auditors_df.iloc():
+            # Adiciona os auditores na tabela auditors do DB.
+            if auditor['name'] not in auditors_set:
+                sql_script = """
+                            INSERT INTO auditors(name) VALUES (?);
+                            """
+                cursor.execute(sql_script, [auditor['name']])
+                db.commit()
+            sql_script = """
+            INSERT INTO auditors_issues(auditor_id, issue_id) VALUES (?, ?);
+            """
+            cursor.execute(sql_script, [API.get_auditor_id(auditor['name']), auditor['issue_id']])
+            auditors_set.add(auditor['name'])
+
+        for finding in findings_df.iloc():
             # Adiciona os findings na tabela findings do DB.
             sql_script = """
-            INSERT INTO findings(title, severity, auditor_id) VALUES (?, ?, ?);
+            INSERT INTO findings(title, severity, auditor_id, issue_id) VALUES (?, ?, ?, ?);
             """
-            cursor.execute(sql_script, [title, severity, get_auditor_id(auditor, db_path)])
+            cursor.execute(sql_script, finding.values)
             db.commit()
 
     return True
-
-
-def get_auditor_id(name: str, db_path: str) -> int:
-    """
-    Função utilizada para encontrar o ID dos auditores através do nome dado como parâmetro.
-
-    :param name: Nome do auditor.
-    :param db_path: Local do banco de dados.
-    :return: ID inteiro do auditor.
-    """
-
-    with sqlite3.connect(db_path) as db:
-        # Conecta ao banco de dados.
-        cursor = db.cursor()
-        # Seleciona o id de acordo com o nome do auditor.
-        cursor.execute("SELECT id FROM auditors WHERE name = ?", [name])
-        # Retorna o valor encontrado pelo SELECT acima.
-        auditor_id = cursor.fetchone()
-        return auditor_id[0]
 
 
 class API:
@@ -142,6 +169,24 @@ class API:
                 return auditors
             else:
                 return []
+
+    @classmethod
+    def get_auditor_id(cls, name: str) -> int:
+        """
+        Função utilizada para encontrar o ID dos auditores através do nome dado como parâmetro.
+
+        :param name: Nome do auditor.
+        :return: ID inteiro do auditor.
+        """
+
+        with sqlite3.connect(cls.DB_PATH) as db:
+            # Conecta ao banco de dados.
+            cursor = db.cursor()
+            # Seleciona o id de acordo com o nome do auditor.
+            cursor.execute("SELECT id FROM auditors WHERE name = ?", [name])
+            # Retorna o valor encontrado pelo SELECT acima.
+            auditor_id = cursor.fetchone()
+            return auditor_id[0]
 
     @classmethod
     def get_findings_by_auditors(cls, *auditors: str | int) -> list[dict]:
@@ -262,6 +307,109 @@ class API:
             return cursor.fetchall()
 
     @classmethod
+    def get_dataframe(cls, script: str) -> pd.DataFrame:
+        """
+        Método para retornar um dataframe pandas de acordo com o comando SQL passado como parâmetro
+
+        :param script: Comando SQL.
+        :return: Dataframe com o resultado da busca no DB
+        """
+
+        with sqlite3.connect(cls.DB_PATH) as db:
+            dataframe = pd.read_sql_query(script, db)
+            return dataframe
+
+    @classmethod
+    def get_issues(cls, **kwargs) -> pd.DataFrame | str | int | list[str | int]:
+        """
+        Método para retornar issues de acordo com os pares de valor passados.
+        possíveis keywords:
+        - auditor_id = int
+        - auditor_ids = int[]
+        - auditor_name = str
+        - auditor_names = str[]
+        - issue_id = int
+        - issue_ids = int[]
+        - issue_title = str
+        - issue_titles = str[]
+
+        TODO: issue_id implementation
+        TODO: issue_ids implementation
+        TODO: issue_title implementation
+        TODO: issue_titles implementation
+
+        :param kwargs: Par chave valor para busca do(s) resultado(s).
+        :return: DataFrame com issues e nomes dos auditores se não houver parâmetros,
+                 DataFrame com issues onde auditor_id(s) ou auditor_name(s) forem encontrados,
+                 título da issue se passado keyword issue_id,
+                 id da issue se passado keyword issue_title,
+                 lista com títulos das issues se passado keyword issue_ids,
+                 lista com ids das issues se passado keyword issue_titles.
+        """
+
+        if not kwargs:
+            with sqlite3.connect(cls.DB_PATH) as db:
+                script = """
+                                SELECT * FROM
+                                    (SELECT issue.* FROM issues issue
+                                        INNER JOIN auditors_issues ai on issue.id = ai.issue_id)
+                                    LEFT JOIN
+                                    (SELECT ad.* FROM auditors ad
+                                        INNER JOIN auditors_issues ai on ad.id = ai.auditor_id) as auditor ON auditor.id;
+                                """
+
+                df = pd.read_sql_query(script, db)
+                return df
+        if "auditor_id" in kwargs:
+            auditor_id = None
+            try:
+                auditor_id = kwargs['auditor_id']
+            except KeyError:
+                pass
+
+            with sqlite3.connect(cls.DB_PATH) as db:
+                script = f"""
+                                SELECT * FROM (SELECT * FROM (SELECT * FROM auditors WHERE id = {auditor_id}) as auditor
+                                     INNER JOIN auditors_issues ai on auditor.id = ai.auditor_id) as si
+                                        INNER JOIN issues iss on si.issue_id = iss.id;
+                                """
+                df = pd.read_sql_query(script, db)
+                return df
+
+        elif "auditor_ids" in kwargs:
+            with sqlite3.connect(cls.DB_PATH) as db:
+                auditor_ids = tuple(kwargs['auditor_ids'])
+                script = f"""
+                                SELECT * FROM (SELECT * FROM (SELECT * FROM auditors WHERE id IN {auditor_ids}) as auditor
+                                     INNER JOIN auditors_issues ai on auditor.id = ai.auditor_id) as si
+                                        INNER JOIN issues iss on si.issue_id = iss.id;
+                                """
+                df = pd.read_sql_query(script, db)
+                return df
+
+        elif "auditor_name" in kwargs:
+            auditor_name = kwargs['auditor_name']
+            with sqlite3.connect(cls.DB_PATH) as db:
+                script = f"""
+                                SELECT * FROM (SELECT * FROM (SELECT * FROM auditors WHERE name = '{auditor_name}') as auditor
+                                     INNER JOIN auditors_issues ai on auditor.id = ai.auditor_id) as si
+                                        INNER JOIN issues iss on si.issue_id = iss.id;
+                                """
+                df = pd.read_sql_query(script, db)
+                return df
+
+        elif "auditor_names" in kwargs:
+            with sqlite3.connect(cls.DB_PATH) as db:
+                auditor_names = tuple(kwargs['auditor_names'])
+                script = f"""
+                                SELECT * FROM (SELECT * FROM (SELECT * FROM auditors WHERE name IN {auditor_names}) as auditor
+                                     INNER JOIN auditors_issues ai on auditor.id = ai.auditor_id) as si
+                                        INNER JOIN issues iss on si.issue_id = iss.id;
+                                """
+                df = pd.read_sql_query(script, db)
+                return df
+
+    @classmethod
     def db_path(cls, path: str = None) -> str:
         """
         Muda e/ou retorna o caminho para o banco de dados.
@@ -284,4 +432,5 @@ class API:
 
 
 if __name__ == '__main__':
-    json_to_sql('../../data/resources/JSON/output.json', '../../data/resources/DB/output.db', True)
+    API.db_path('../../data/resources/DB/output.db')
+    # json_to_sql('../../data/resources/JSON/output.json', '../../data/resources/DB/output.db', True)
