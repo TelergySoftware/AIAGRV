2087e785fef5daaa64ee8d48143875047c37f6cc
Matheus Santos <matheus.santos@telergystudio.com>
2022-02-06 18:25:38 -0300

    [CODE] — Removido classe Parser e adicionado função `words_from_json`.

--- sources/utils/src/data_parser.py
+++ sources/utils/src/data_parser.py
@@ -1,34 +1,17 @@
 from __future__ import annotations
+from json_converter import convert
+from CPP.shared import ICdataUtils
 
 
-class Parser:
-    @classmethod
-    def to_percent(cls, values: float | int | list[float | int],
-                   totals: float | int | list[float | int]) -> float | int | list[float | int]:
-        """
-        Método para conversão de valores para porcentagem.
-
-        :param values: Valor ou valores para serem convertidos.
-        :param totals: Valor ou valores do total.
-        :return: Valor ou valores convertidos para porcentagem.
-        """
-
-        if type(values) in [int, float]:
-            return (values / totals) * 100
-        elif type(values) == list:
-            return [(value / total) * 100 for value, total, in zip(values, totals)]
-        else:
-            print("Campos values e totals devem ser int, float ou list!")
-            return -1
-
-    @classmethod
-    def to_percent_string(cls, value: float | int, total: float | int) -> str:
-        """
-        Método para conversão de valores para porcentagem.
-
-        :param value: Valor ou valores para serem convertidos.
-        :param total: Valor ou valores do total.
-        :return: String to valor em porcento.
-        """
-
-        return f'{(value / total) * 100:.2f}%'
+def words_from_json(json: str | list | dict):
+    if type(json) == str:
+        js = convert(json)
+        if type(js) == dict:
+            js = [js]
+        return ICdataUtils.countWords(js)
+    elif type(json) == list:
+        return ICdataUtils.countWords(json)
+    elif type(json) == dict:
+        return ICdataUtils.countWords([json])
+    else:
+        print(f"JSON must be a str, list or dict you passed a {type(json)}")
